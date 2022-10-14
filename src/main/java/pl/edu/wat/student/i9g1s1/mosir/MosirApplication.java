3a21af97a4b5c2e9cd715b6c228126d8f046e2d0
kuna7 <kuna728@gmail.com>
2022-10-14 13:58:48 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wat/student/i9g1s1/mosir/MosirApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.student.i9g1s1.mosir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wat/student/i9g1s1/mosir/MosirApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.student.i9g1s1.mosir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
